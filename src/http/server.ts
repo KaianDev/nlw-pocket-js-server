e4100c1f00e9ea86d116cf0e004268f6621f625e
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2024-09-09 20:10:27 -0300

    feat: server is running

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,1 +1,12 @@
+import fastify from "fastify"
 
+const app = fastify()
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("🚀 HTTP Server running")
+    console.log("🔗 http://localhost:3333")
+  })
